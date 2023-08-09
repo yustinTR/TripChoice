3cecec77c1caba5d25558615b5b58df29d63974f
yustin troost <yustin.troost@finalist.nl>
2023-08-09 21:24:22 +0200

    Make homepage

--- web/sites/default/settings.ddev.php
+++ web/sites/default/settings.ddev.php
@@ -53,8 +53,11 @@ $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml'
 $config['system.performance']['css']['preprocess'] = FALSE;
 $config['system.performance']['js']['preprocess'] = FALSE;
 
-$config['system.logging']['error_level'] = 'verbose';
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['page'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
 
+$config['system.logging']['error_level'] = 'verbose';
 
 /**
  * Override solr server.
--- web/themes/custom/tripchoice/dist/js/hamburger.js
+++ web/themes/custom/tripchoice/dist/js/hamburger.js
@@ -15,6 +15,13 @@
 (function ($, Drupal, window, document, undefined) {
   $(".mobile-service-toggle").click(function () {
     $('.mobile-navigation-region').toggleClass('open');
+    if ($('.mobile-navigation-region').hasClass('open')) {
+      $(".hamburger--icon-open").hide();
+      $(".hamburger--icon-close").show();
+    } else {
+      $(".hamburger--icon-close").hide();
+      $(".hamburger--icon-open").show();
+    }
   });
 })(jQuery, Drupal, this, this.document);
 
